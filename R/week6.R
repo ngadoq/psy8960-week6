9689f553b5ccca3b22916d0369885ffdd6b5a7e6
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-02-26 23:18:07 -0600

    Data import

--- R/week6.R
+++ R/week6.R
@@ -2,11 +2,13 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
 library(stringr)
-library(readr)
+
 
 
 # Data Import
 citations <- stri_read_lines("../data/citations.txt")
-citations_txt <- str_match(citations, "^$")
-citations_txt <- read_lines(citations, skip_empty_rows = TRUE)
+citations_txt <- str_subset(citations, ".$")
+length(citations)-length(citations_txt)
+mean(str_length(citations_txt))
+
 
