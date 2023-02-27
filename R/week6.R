d4a86f55d217d9b84c6ae555c0919c22f6b0a5d5
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-02-27 10:23:45 -0600

    Data Cleaning

--- R/week6.R
+++ R/week6.R
@@ -16,16 +16,11 @@ sample(citations_txt, size = 10)
 original = citations_tbl <- as_tibble(citations_txt, rownames = "line", .name_repair =~ "cite")
 citations_tbl <- as_tibble(citations_txt, rownames = "line", .name_repair =~ "cite") |> 
   mutate(cite = str_replace_all(cite, "(\"|'')", ""),
-         year = str_extract(pattern = "(\\d+)"),
-         page_start = 
-         perf_ref = str_detect(citations_txt, pattern = "peformance|Performance"),
-         title = 
-         first_author = 
-         )
+         year = str_extract(cite, pattern = "(\\d{4})"),
+         page_start = str_match(cite, pattern = "(\\d+)-\\d+")[,2],
+         perf_ref = str_detect(cite, pattern = "peformance|Performance"),
+         title = str_match(cite, pattern = ".*\\(\\d{4}\\)\\.\\s([^.]*\\.)(?: .*)")[,2],
+         first_author = str_extract(cite, pattern = "([\\sa-zA-Z,]+)."))
 
-
-#.([\sa-zA-Z,:\n\t]+).
-
-str_match(citations_txt, pattern = "[a-zA-Z.&\\s,]+?\\(\\d+\\).([\\sa-zA-Z,:\n\t]+).")
-
+length(citations_tbl$first_author)
 
